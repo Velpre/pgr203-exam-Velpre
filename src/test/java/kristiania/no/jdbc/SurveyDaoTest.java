dab4003932e08827b8cf86aae35de886025c995d
Velpre <veljko_kv@hotmail.com>
2021-11-03 15:49:56 +0100

    right commit after merge

--- src/test/java/kristiania/no/jdbc/SurveyDaoTest.java
+++ src/test/java/kristiania/no/jdbc/SurveyDaoTest.java
@@ -3,26 +3,30 @@ package kristiania.no.jdbc;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SurveyDaoTest {
     private SurveyDao dao = new SurveyDao(TestData.testDataSource());
 
-    /*
+
     @Test
-    void shouldListSavedRoles() throws SQLException {
-        Survey survey1 = "survey-" + UUID.randomUUID();
-        Survey survey2 = "survey-" + UUID.randomUUID();
-
-        dao.save(survey1);
-        dao.save(survey2);
-
-        assertThat(dao.listAll())
-                .extracting(Survey::getName)
-                .contains(survey1, survey2);
+    void shouldRetrieveSavedSurvey() throws SQLException {
+        Survey survey = new Survey("s1");
+        dao.save(survey);
+        assertThat(dao.retrieve(survey.getId()))
+                .hasNoNullFieldsOrProperties()
+                .usingRecursiveComparison()
+                .isEqualTo(survey);
     }
 
-     */
+    @Test
+    void shouldListSavedSurvey() throws SQLException {
+        // survey blir opprettet i V004 migration
+        assertThat(dao.listAll())
+                .extracting(Survey::getName)
+                .contains("survey1", "survey2");
+    }
+
+
 }
