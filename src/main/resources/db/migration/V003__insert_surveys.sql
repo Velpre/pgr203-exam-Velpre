bf581f11a741314ea4a61f4637629e5bcb141176
andersholt <anho068@student.kristiania.no>
2021-11-03 14:50:51 +0100

    fixed tests

--- src/main/resources/db/migration/V003__insert_surveys.sql
+++ src/main/resources/db/migration/V003__insert_surveys.sql
@@ -1,4 +1,2 @@
 insert into survey (survey_name) values ('survey1');
 insert into survey (survey_name) values ('survey2');
-insert into survey (survey_name) values ('survey3');
-insert into survey (survey_name) values ('survey4');
--- src/main/resources/db/migration/V004__insert_questions.sql
+++ src/main/resources/db/migration/V004__insert_questions.sql
@@ -1,3 +1,2 @@
 insert into questions (title, question_text, survey_id) values ('question1','content', 1);
-insert into questions (title, question_text, survey_id) values ('question1','content', 2);
-insert into questions (title, question_text, survey_id) values ('question1','content', 3);
+insert into questions (title, question_text, survey_id) values ('question2','content', 2);
